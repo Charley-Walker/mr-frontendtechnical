f8d843673996fa38e5e56f9de5cdad894e49b1e1
Charley Walker <61216530+Charley-Walker@users.noreply.github.com>
2020-11-10 16:07:38 +1300

    Product base elements added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header'
+import Product from './Components/Product'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Product />
     </div>
   );
 }
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import productImage from '../images/classic-tee.jpg'
 
 function Product() {
     return (
         <div className="mr-product-container">
+            <div className="mr-product-imageContainer">
+                <img src={productImage} alt="Product Image" className="mr-product-image"/>
+            </div>
             <div>
+                <h1>Classic Tee</h1>
+                <p className="mr-bold">$75.00</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at neque et lacus consequat euismod ut id eros. Aliquam finibus ligula dictum risus feugiat, sit amet consectetur tellus posuere. In magna quam, rutrum a posuere et, dignissim ut nulla. Mauris sit amet commodo dolor. Nam sapien nulla, mollis ac varius eu, feugiat vel est. Praesent a nisi iaculis, tempor libero sed, tristique nunc. Maecenas luctus tempor eleifend. Aenean porta molestie finibus.</p>
+                <div className="mr-flex">
+                    <p className="mr-upper">Size</p>
+                    <span>*</span>
+                    <p></p>
+                </div>
+                <div>
+                    <button>S</button>
+                    <button>M</button>
+                    <button>L</button>
+                </div>
+                <button className="mr-upper">Add To Cart</button>
                 
             </div>
         </div>
